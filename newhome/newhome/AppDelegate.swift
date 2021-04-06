c54a0a1448ea0df235b4c937b3f4666713226ffb
nsdom <nico.domingues.dev@gmail.com>
2021-04-07 00:15:32 +0100

    project setup

---
+++ newhome/newhome/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  newhome
+//
+//  Created by Nicolá Domingues on 07/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+
+}
+
---
+++ newhome/newhome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newhome
+//
+//  Created by Nicolá Domingues on 07/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+    }
+
+
+}
+
